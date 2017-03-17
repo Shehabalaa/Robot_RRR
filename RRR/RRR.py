209ed37b15578db5e5da89304dd629511e9db5aa
Shehab <Shehabalaa97@gmail.com>
2017-03-17 13:34:23 +0200

    d

--- RRR/RRR.py
+++ RRR/RRR.py
@@ -8,43 +8,67 @@ def Rad(degree):
 
 
 def workspace():
-    q1_st = 0.0;
-    q2_st = 0.0;
+    q1_st = 0.0
+    q2_st = 0.0
     q3_st = 0.0
-    q1_end = 0.0;
-    q2_end = 0.0;
+    q1_end = 0.0
+    q2_end = 0.0
     q3_end = 0.0
-    l1 = 0.0;
-    l2 = 8.0;
+    l1 = 0.0
+    l2 = 8.0
     l3 = .0
 
     f = open("Data.txt", "r")
 
-    l1 = f.readline();
+    l1 = f.readline()
     l1 = float(l1)
-    l2 = f.readline();
+    l2 = f.readline()
     l2 = float(l2)
-    l3 = f.readline();
+    l3 = f.readline()
     l3 = float(l3)
-    q1_st = f.readline();
+    q1_st = f.readline()
     q1_st = float(q1_st)
-    q1_end = f.readline();
+    q1_end = f.readline()
     q1_end = float(q1_end)
-    q2_st = f.readline();
+    q2_st = f.readline()
     q2_st = float(q2_st)
-    q2_end = f.readline();
+    q2_end = f.readline()
     q2_end = float(q2_end)
-    q3_st = f.readline();
+    q3_st = f.readline()
     q3_st = float(q3_st)
-    q3_end = f.readline();
+    q3_end = f.readline()
     q3_end = float(q3_end)
+    if abs(q1_end - q1_st) >= 180:
+        range1 = abs(q1_end - q1_st) / 90
+    elif abs(q1_end - q1_st) >=90:
+        range1 = abs(q1_end - q1_st) / 45
+    else:
+        range1 = 1
 
+    if abs(q2_end - q2_st) >= 180:
+        range2 = abs(q2_end - q2_st) / 90
+    elif abs(q2_end - q2_st) >=90:
+        range2= abs(q2_end - q2_st) / 45
+    else:
+        range2 = 1
+
+    if abs(q3_end - q3_st) >= 180:
+        range3 = abs(q3_end - q3_st) / 90
+    elif abs(q3_end - q3_st) >= 90:
+        range3 = abs(q3_end - q3_st) / 45
+    else:
+        range3 = 1
+
+    range1 = math.floor(range1)
+    range2 = math.floor(range2)
+    range3 = math.floor(range3)
     x = []
     y = []
-    alpha = plt.figure()
-    for i in range(int(q1_st), int(q1_end)):
-        for j in range(int(q2_st), int(q2_end)):
-            for k in range(int(q3_st), int(q3_end)):
+
+
+    for i in range(int(q1_st), int(q1_end), range1):
+        for j in range(int(q2_st), int(q2_end), range2):
+            for k in range(int(q3_st), int(q3_end), range3):
                 x.append(l1 * math.cos(Rad(i)) + l2 * math.cos(Rad(i + j)) + l3 * math.cos(Rad(i + j + k)))
                 y.append(l1 * math.sin(Rad(i)) + l2 * math.sin(Rad(i + j)) + l3 * math.sin(Rad(i + j + k)))
 
